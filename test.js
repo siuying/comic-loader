87683e6469e83e793332f061557e2b42d2f995af
Francis Chong <francis@ignition.hk>
2014-08-01 18:03:28 +0800

    extract the url.

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var phridge = require('phridge')
 phridge.spawn().then(function(phantom){
-  phantom.run("#stream_title", function (selector, resolve) {
+  phantom.run("http://www.theverge.com/2013/1/7/3835724/the-price-of-apps", "#stream_title", function (url, selector, resolve) {
       var page = webpage.create();
-      page.open("http://www.theverge.com/2013/1/7/3835724/the-price-of-apps", function () {
+      page.open(url, function () {
           var text = page.evaluate(function (selector) {
               return document.querySelector(selector).innerText;
           }, selector);
